72cc3266b3819fc82ef7c7037a1aef591d2eb87c
djhern <129998408+djhern@users.noreply.github.com>
2023-04-13 00:30:10 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+function showDiv(id) {
 
+    document.getElementById(id).style.display = "block";
+
+    const divs = ["Div1", "Div2", "Div3", "Div4"];
+
+    const index = divs.indexOf(id);
+
+    divs.splice(index, 1);
+
+    for (let i = 0; i < divs.length; i++) {
+
+        document.getElementById(divs[i]).style.display = 'none';
+    }
+}
+
+function openTab(url) {
+    window.open(url, "_blank");
+}
